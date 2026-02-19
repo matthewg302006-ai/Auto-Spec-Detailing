59f57bb6010f6e02bc2b753da239976849f787f0
matthewg302006-ai <matthewg302006@gmail.com>
2026-02-19 22:08:18 +0200

    Added postcss.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,12 +6,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  swcMinify: true,
   reactStrictMode: true,
-  onDemandEntries: {
-    maxInactiveAge: 60 * 1000,
-    pagesBufferLength: 5,
-  },
 }
 
 export default nextConfig
